5b985439f2f0f4c75d366e77028b2f9ee57f6fb4
Gustavo032 <gustavoramosrock@gmail.com>
2023-07-21 17:59:09 -0300

    criação de usuário

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
-import { PrismaClient } from '@prisma/client'
+import { prisma } from './lib/prisma'
 import fastify from 'fastify'
+import { z } from 'zod'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
 
-prisma.user.create({
-  data: { nome: 'gustavo', email: 'gustavo@prisma.comf' },
+  const { name, email, password } = registerBodySchema.parse(request.body)
+  // throw automático no erro se essa validação falhar, nenhum código roda se falhar
+
+  await prisma.user.create({
+    data: {
+      nome: name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
 })
