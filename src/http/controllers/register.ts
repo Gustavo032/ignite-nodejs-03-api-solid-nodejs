72a78ed944d7355be6d7366f72a550b7a7515d14
Gustavo032 <gustavoramosrock@gmail.com>
2023-07-26 11:52:39 -0300

    hash de senha e validação email

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
 
@@ -8,15 +9,27 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     email: z.string().email(),
     password: z.string().min(6),
   })
+  // $2a$06$ZkvspiooheklSO3jydiw3ugE5xgphJ.KKZX9x08MLIpmAzkarlxXa
 
   const { name, email, password } = registerBodySchema.parse(request.body)
   // throw automático no erro se essa validação falhar, nenhum código roda se falhar
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
 
   await prisma.user.create({
     data: {
       nome: name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
 
