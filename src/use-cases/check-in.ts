6a08aaf444d5d10a4882ee6d63ffc0beca1a5333
Gustavo032 <gustavoramosrock@gmail.com>
2024-01-12 07:30:19 -0300

    red, green, refactor // TDD Mocking

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -17,8 +17,15 @@ export class CheckInUseCase{
 	
 	async execute({userId, gymId}: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse>{
 		
+		const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(userId, new Date());
+
+		if(checkInOnSameDate){
+
+			throw new Error()
+		}
 		const checkIn = await this.checkInsRepository.create({gym_id: gymId, user_id: userId});
 
+		
 		return {
 			checkIn
 		}
